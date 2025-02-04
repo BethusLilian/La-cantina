c475bf20982c92b2b00c12d2b28ba28b5c05676c
LilianB <lilian.bths@gmail.com>
2025-02-04 15:43:46 +0100

    nouveau footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccueilComponent } from "./components/accueil/accueil.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AccueilComponent],
+  imports: [RouterOutlet, AccueilComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
